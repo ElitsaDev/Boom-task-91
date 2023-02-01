26e8cd14ac0b1ae6bb265c64844f4c7ad2f40e8c
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-02-02 00:37:11 +0200

    using EventEmitter from "eventemitter3"

--- src/js/Application.js
+++ src/js/Application.js
@@ -2,24 +2,32 @@ import EventEmitter from "eventemitter3";
 import Beat from "./Beat";
 
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+    static get events() {
+        return {
+            READY: "ready",
+        };
+    }
 
-  constructor() {
-    super();
+    constructor() {
+        super();
+        this._beat = new Beat();
+        const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+        let count = 0;
 
-    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
-    let count = 0;
+        this._beat.addListener(Beat.events.BIT, () => {
+            if (count > (lyrics.length - 1)) {
+                count = 0;
+            }
+            this._create(lyrics[count++]);
+        });
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
+        this.emit(Application.events.READY);
+    }
 
-    document.querySelector(".main").appendChild(message);
-
-    this.emit(Application.events.READY);
-  }
+    _create(messageText) {
+        const message = document.createElement("div");
+        message.classList.add("message");
+        message.innerText = messageText;
+        document.querySelector(".main").appendChild(message);
+    }
 }
